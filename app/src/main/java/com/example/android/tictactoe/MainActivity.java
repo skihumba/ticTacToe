890aedca0359947bf0bd831641b2e4ea4e2c7733
Skihumba <abmuhiks@gmail.com>
2018-04-09 11:46:42 +0300

    edited activity_mail.xml and MainActivity.java

--- app/src/main/java/com/example/android/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/android/tictactoe/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //            reset button OnClickListener
             @Override
             public void onClick(View v) {
+                resetGame();
 
             }
         });
@@ -161,6 +162,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         roundCount = 0;
         player1Turn = true;
+
+    }
+
+    private void resetGame(){
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("player1Points", player1Points);
+        outState.putInt("player2Points", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getByte("player1Points");
+        player2Points = savedInstanceState.getByte("player2Points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
 
     }
 }
