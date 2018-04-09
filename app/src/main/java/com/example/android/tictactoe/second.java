c527d8052abeebcc8f8fd1ad13f05a53e6c67e02
Skihumba <abmuhiks@gmail.com>
2018-04-10 01:48:24 +0300

    edited second.xml

--- app/src/main/java/com/example/android/tictactoe/second.java
+++ app/src/main/java/com/example/android/tictactoe/second.java
@@ -1,13 +1,34 @@
 package com.example.android.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class second extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button buttonSwitch = (Button) findViewById(R.id.switch_board_to3_btn);
+        buttonSwitch.setOnClickListener (new View.OnClickListener() {
+
+            //            switch button OnClickListener
+            @Override
+            public void onClick(View v) {
+                switchBoard();
+
+            }
+        });
+
+    }
+
+    private void switchBoard(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
